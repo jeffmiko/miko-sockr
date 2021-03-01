030541672d2f58910ab9111509df69ccae020f52
jeffmiko <jeff.miko@gmail.com>
2021-03-01 08:05:11 -0500

    Added redis app to exports

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { SockrHooks, SockrHookMethods } = require("./lib/SockrHooks")
 const { SockrService } = require("./lib/SockrService")
 const { SockrApp } = require("./lib/SockrApp")
+const { SockrRedisApp } = require("./lib/SockrRedisApp")
 
 function create(options = {}) {
   const app = new SockrApp(options)
@@ -9,6 +10,7 @@ function create(options = {}) {
 
 module.exports = create
 module.exports.SockrApp = SockrApp
+module.exports.SockrRedisApp = SockrRedisApp
 module.exports.SockrService = SockrService
 module.exports.SockrHooks = SockrHooks
 module.exports.SockrHookMethods = SockrHookMethods
