97984094c3067547d2d7cee80f8b971b516fd686
jeffmiko <jeff.miko@gmail.com>
2021-03-01 12:54:44 -0500

    Adding before and after hooks

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { SockrHooks, SockrHookMethods } = require("./lib/SockrHooks")
 const { SockrService } = require("./lib/SockrService")
 const { SockrApp } = require("./lib/SockrApp")
 const { SockrRedisApp } = require("./lib/SockrRedisApp")
+const { SockrHookUtils, SockrAfterHooks, SockrBeforeHooks } = require("./lib/SockrHookUtils")
 
 function create(options = {}) {
   const app = new SockrApp(options)
@@ -14,4 +15,8 @@ module.exports.SockrRedisApp = SockrRedisApp
 module.exports.SockrService = SockrService
 module.exports.SockrHooks = SockrHooks
 module.exports.SockrHookMethods = SockrHookMethods
+
+module.exports.SockrHookUtils = SockrHookUtils
+module.exports.SockrAfterHooks = SockrAfterHooks
+module.exports.SockrBeforeHooks = SockrBeforeHooks
 
